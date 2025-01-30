c7dcde7587ac20d6e0e5a775eddbab2dae343f3e
Ahmed A <163431268+ahmed-almouna@users.noreply.github.com>
2025-01-30 16:45:59 -0500

    Added reliable UDP content

--- ReliableUDP/ReliableUDP.cpp
+++ ReliableUDP/ReliableUDP.cpp
@@ -1,20 +1,280 @@
-// ReliableUDP.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+/*
+	Reliability and Flow Control Example
+	From "Networking for Game Programmers" - http://www.gaffer.org/networking-for-game-programmers
+	Author: Glenn Fiedler <gaffer@gaffer.org>
+*/
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
-int main()
+#include "Net.h"
+
+//#define SHOW_ACKS
+
+using namespace std;
+using namespace net;
+
+const int ServerPort = 30000;
+const int ClientPort = 30001;
+const int ProtocolId = 0x11223344;
+const float DeltaTime = 1.0f / 30.0f;
+const float SendRate = 1.0f / 30.0f;
+const float TimeOut = 10.0f;
+const int PacketSize = 256;
+
+class FlowControl
 {
-    std::cout << "Hello World!\n";
+public:
+
+	FlowControl()
+	{
+		printf("flow control initialized\n");
+		Reset();
+	}
+
+	void Reset()
+	{
+		mode = Bad;
+		penalty_time = 4.0f;
+		good_conditions_time = 0.0f;
+		penalty_reduction_accumulator = 0.0f;
+	}
+
+	void Update(float deltaTime, float rtt)
+	{
+		const float RTT_Threshold = 250.0f;
+
+		if (mode == Good)
+		{
+			if (rtt > RTT_Threshold)
+			{
+				printf("*** dropping to bad mode ***\n");
+				mode = Bad;
+				if (good_conditions_time < 10.0f && penalty_time < 60.0f)
+				{
+					penalty_time *= 2.0f;
+					if (penalty_time > 60.0f)
+						penalty_time = 60.0f;
+					printf("penalty time increased to %.1f\n", penalty_time);
+				}
+				good_conditions_time = 0.0f;
+				penalty_reduction_accumulator = 0.0f;
+				return;
+			}
+
+			good_conditions_time += deltaTime;
+			penalty_reduction_accumulator += deltaTime;
+
+			if (penalty_reduction_accumulator > 10.0f && penalty_time > 1.0f)
+			{
+				penalty_time /= 2.0f;
+				if (penalty_time < 1.0f)
+					penalty_time = 1.0f;
+				printf("penalty time reduced to %.1f\n", penalty_time);
+				penalty_reduction_accumulator = 0.0f;
+			}
+		}
+
+		if (mode == Bad)
+		{
+			if (rtt <= RTT_Threshold)
+				good_conditions_time += deltaTime;
+			else
+				good_conditions_time = 0.0f;
+
+			if (good_conditions_time > penalty_time)
+			{
+				printf("*** upgrading to good mode ***\n");
+				good_conditions_time = 0.0f;
+				penalty_reduction_accumulator = 0.0f;
+				mode = Good;
+				return;
+			}
+		}
+	}
+
+	float GetSendRate()
+	{
+		return mode == Good ? 30.0f : 10.0f;
+	}
+
+private:
+
+	enum Mode
+	{
+		Good,
+		Bad
+	};
+
+	Mode mode;
+	float penalty_time;
+	float good_conditions_time;
+	float penalty_reduction_accumulator;
+};
+
+// ----------------------------------------------
+
+int main(int argc, char* argv[])
+{
+	// parse command line
+
+	enum Mode
+	{
+		Client,
+		Server
+	};
+
+	Mode mode = Server;
+	Address address;
+
+	if (argc >= 2)
+	{
+		int a, b, c, d;
+#pragma warning(suppress : 4996)
+		if (sscanf(argv[1], "%d.%d.%d.%d", &a, &b, &c, &d))
+		{
+			mode = Client;
+			address = Address(a, b, c, d, ServerPort);
+		}
+	}
+
+	// initialize
+
+	if (!InitializeSockets())
+	{
+		printf("failed to initialize sockets\n");
+		return 1;
+	}
+
+	ReliableConnection connection(ProtocolId, TimeOut);
+
+	const int port = mode == Server ? ServerPort : ClientPort;
+
+	if (!connection.Start(port))
+	{
+		printf("could not start connection on port %d\n", port);
+		return 1;
+	}
+
+	if (mode == Client)
+		connection.Connect(address);
+	else
+		connection.Listen();
+
+	bool connected = false;
+	float sendAccumulator = 0.0f;
+	float statsAccumulator = 0.0f;
+	int helloWorldCounter = 1;
+
+	FlowControl flowControl;
+
+	while (true)
+	{
+		// update flow control
+
+		if (connection.IsConnected())
+			flowControl.Update(DeltaTime, connection.GetReliabilitySystem().GetRoundTripTime() * 1000.0f);
+
+		const float sendRate = flowControl.GetSendRate();
+
+		// detect changes in connection state
+
+		if (mode == Server && connected && !connection.IsConnected())
+		{
+			flowControl.Reset();
+			printf("reset flow control\n");
+			connected = false;
+		}
+
+		if (!connected && connection.IsConnected())
+		{
+			printf("client connected to server\n");
+			connected = true;
+		}
+
+		if (!connected && connection.ConnectFailed())
+		{
+			printf("connection failed\n");
+			break;
+		}
+
+		// send and receive packets
+
+		sendAccumulator += DeltaTime;
+
+		while (sendAccumulator > 1.0f / sendRate)
+		{
+			unsigned char packet[PacketSize];
+			memset(packet, 0, sizeof(packet)); //clear
+
+			char newPacket[PacketSize];
+			sprintf(newPacket, "Hello World <<%d>>", helloWorldCounter);
+
+			sprintf((char*)packet, newPacket);
+			connection.SendPacket(packet, sizeof(packet));
+			sendAccumulator -= 1.0f / sendRate;
+
+			memset(newPacket, 0, sizeof(newPacket)); //clear
+			helloWorldCounter++;
+		}
+
+		while (true)
+		{
+			unsigned char packet[256];
+			int bytes_read = connection.ReceivePacket(packet, sizeof(packet));
+			if (bytes_read == 0)
+				break;
+			printf("%s", packet);//
+		}
+
+		// show packets that were acked this frame
+
+#ifdef SHOW_ACKS
+		unsigned int* acks = NULL;
+		int ack_count = 0;
+		connection.GetReliabilitySystem().GetAcks(&acks, ack_count);
+		if (ack_count > 0)
+		{
+			printf("acks: %d", acks[0]);
+			for (int i = 1; i < ack_count; ++i)
+				printf(",%d", acks[i]);
+			printf("\n");
+		}
+#endif
+
+		// update connection
+
+		connection.Update(DeltaTime);
+
+		// show connection stats
+
+		statsAccumulator += DeltaTime;
+
+		while (statsAccumulator >= 0.25f && connection.IsConnected())
+		{
+			float rtt = connection.GetReliabilitySystem().GetRoundTripTime();
+
+			unsigned int sent_packets = connection.GetReliabilitySystem().GetSentPackets();
+			unsigned int acked_packets = connection.GetReliabilitySystem().GetAckedPackets();
+			unsigned int lost_packets = connection.GetReliabilitySystem().GetLostPackets();
+
+			float sent_bandwidth = connection.GetReliabilitySystem().GetSentBandwidth();
+			float acked_bandwidth = connection.GetReliabilitySystem().GetAckedBandwidth();
+
+			printf("rtt %.1fms, sent %d, acked %d, lost %d (%.1f%%), sent bandwidth = %.1fkbps, acked bandwidth = %.1fkbps\n",
+				rtt * 1000.0f, sent_packets, acked_packets, lost_packets,
+				sent_packets > 0.0f ? (float)lost_packets / (float)sent_packets * 100.0f : 0.0f,
+				sent_bandwidth, acked_bandwidth);
+
+			statsAccumulator -= 0.25f;
+		}
+
+		net::wait(DeltaTime);
+	}
+
+	ShutdownSockets();
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
